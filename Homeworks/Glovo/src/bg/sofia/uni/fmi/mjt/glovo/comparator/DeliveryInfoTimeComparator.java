7ec5c9082d00cfed84cbd5d5573f86a8ed41dfa7
Radoslav Karatanev <rmkrmkrmk13@gmail.com>
2024-11-30 17:46:04 +0200

    Refactor

--- Homeworks/Glovo/src/bg/sofia/uni/fmi/mjt/glovo/comparator/DeliveryInfoTimeComparator.java
+++ Homeworks/Glovo/src/bg/sofia/uni/fmi/mjt/glovo/comparator/DeliveryInfoTimeComparator.java
@@ -8,13 +8,11 @@ public class DeliveryInfoTimeComparator implements Comparator<DeliveryInfo> {
 
     @Override
     public int compare(DeliveryInfo d1, DeliveryInfo d2) {
-        // Compare by price first
         int timeComparison = Double.compare(d1.getEstimatedTime(), d2.getEstimatedTime());
         if (timeComparison != 0) {
             return timeComparison;
         }
 
-        // If prices are equal, compare by price
         return Double.compare(d1.getPrice(), d2.getPrice());
     }
 }
