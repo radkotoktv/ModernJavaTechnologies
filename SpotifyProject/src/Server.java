1b187968740f5f1132070ece9ccc018cd237fc82
rkaratan <rkaratan@visteon.com>
2025-02-06 12:51:35 +0200

    Refactoring

---
+++ SpotifyProject/src/Server.java
@@ -0,0 +1,144 @@
+import playlist.Playlist;
+import song.Song;
+import user.User;
+
+import java.io.IOException;
+
+import java.net.InetSocketAddress;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.Selector;
+import java.nio.channels.SelectionKey;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Iterator;
+
+import static constants.Constant.USERS_READER;
+import static constants.Constant.SONGS_READER;
+import static constants.Constant.PLAYLIST_READER;
+import static constants.Constant.USER_WRITER;
+import static constants.Constant.SONG_WRITER;
+import static constants.Constant.PLAYLIST_WRITER;
+import static constants.Constant.PORT;
+import static constants.Constant.BUFFER_SIZE;
+
+public class Server {
+    private static ArrayList<User> users;
+    private static ArrayList<Song> songs;
+    private static ArrayList<Playlist> playlists;
+    private static HashMap<User, ArrayList<Playlist>> data;
+
+    public static void main(String... args) throws IOException {
+        users = USERS_READER.readFromFile();
+        songs = SONGS_READER.readFromFile();
+        playlists = PLAYLIST_READER.readFromFile();
+        createServer();
+    }
+
+    public static void registerUser(User newUser) {
+        if (users.contains(newUser)) {
+            System.out.println("User already exists");
+            return;
+        }
+        users.add(newUser);
+        USER_WRITER.writeToFile(newUser);
+    }
+
+    public static void addSong(Song newSong) {
+        if (songs.contains(newSong)) {
+            System.out.println("Song already exists");
+            return;
+        }
+        songs.add(newSong);
+        SONG_WRITER.writeToFile(newSong);
+    }
+
+    public static void addPlaylist(Playlist newPlaylist) {
+        if (playlists.contains(newPlaylist)) {
+            System.out.println("Playlist already exists");
+            return;
+        }
+        playlists.add(newPlaylist);
+        PLAYLIST_WRITER.writeToFile(newPlaylist);
+    }
+
+    public static String handleCommand(String[] receivedData) {
+        return switch (receivedData[0]) {
+            case "register" -> "You have selected the register option!";
+            case "login" -> "You have selected the login option!";
+            case "disconnect" -> "You have selected the disconnect option!";
+            case "search" -> "You have selected the search option!";
+            case "top" -> "You have selected the top option!";
+            case "create-playlist" -> "You have selected the create-playlist option!";
+            case "add-song-to" -> "You have selected the add-song-to option!";
+            case "show-playlist" -> "You have selected the show-playlist option!";
+            case "play" -> "You have selected the play option!";
+            case "stop" -> "You have selected the stop option!";
+            default -> "Invalid command!";
+        };
+    }
+
+    public static void createServer() throws IOException {
+        try (ServerSocketChannel serverSocket = ServerSocketChannel.open();
+             Selector selector = Selector.open()) {
+
+            serverSocket.bind(new InetSocketAddress(Integer.parseInt(PORT.getValue())));
+            serverSocket.configureBlocking(false);
+            serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("Server started on port " + PORT.getValue());
+
+            while (true) {
+                selector.select();
+                Set<SelectionKey> selectedKeys = selector.selectedKeys();
+                Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+
+                while (keyIterator.hasNext()) {
+                    SelectionKey key = keyIterator.next();
+                    keyIterator.remove();
+
+                    if (key.isAcceptable()) {
+                        SocketChannel clientChannel = serverSocket.accept();
+                        if (clientChannel != null) {
+                            clientChannel.configureBlocking(false);
+                            clientChannel.register(selector, SelectionKey.OP_READ);
+                            System.out.println("Accepted connection from " + clientChannel.getRemoteAddress());
+                        }
+                    } else if (key.isReadable()) {
+                        SocketChannel clientChannel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(Integer.parseInt(BUFFER_SIZE.getValue()));
+                        try {
+                            int bytesRead = clientChannel.read(buffer);
+                            if (bytesRead == -1) {
+                                System.out.println("Client disconnected: " + clientChannel.getRemoteAddress());
+                                clientChannel.close();
+                                key.cancel();
+                                continue;
+                            }
+                            buffer.flip();
+                            byte[] requestData = new byte[buffer.remaining()];
+                            buffer.get(requestData);
+                            String[] receivedData = new String(requestData).split(" ");
+                            try {
+                                String response = handleCommand(receivedData);
+                                buffer.clear();
+                                buffer.put(response.getBytes());
+                                buffer.flip();
+                                clientChannel.write(buffer);
+                            } catch (NumberFormatException e) {
+                                System.out.println("Invalid input received from client: " + receivedData);
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Error handling client: " + e.getMessage());
+                            key.cancel();
+                            clientChannel.close();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- SpotifyProject/src/constants/Constant.java
+++ SpotifyProject/src/constants/Constant.java
@@ -1,8 +1,28 @@
 package constants;
 
+import file.reader.PlaylistReader;
+import file.reader.SongReader;
+import file.reader.UsersReader;
+import file.writer.PlaylistWriter;
+import file.writer.SongWriter;
+import file.writer.UserWriter;
+
 public enum Constant {
     PORT("7777"),
+
     BUFFER_SIZE("1024");
+
+    public static final UsersReader USERS_READER = new UsersReader();
+
+    public static final SongReader SONGS_READER = new SongReader();
+
+    public static final PlaylistReader PLAYLIST_READER = new PlaylistReader();
+
+    public static final UserWriter USER_WRITER = new UserWriter();
+    
+    public static final SongWriter SONG_WRITER = new SongWriter();
+
+    public static final PlaylistWriter PLAYLIST_WRITER = new PlaylistWriter();
 
     private final String value;
 
